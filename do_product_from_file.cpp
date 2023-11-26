4fbd977410cb9870c498c892cd69f8e21c25d240
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2023-11-26 22:35:44 +0000

    Add more option to control the output of do_product_from_file

--- do_product_from_file.cpp
+++ do_product_from_file.cpp
@@ -8,8 +8,8 @@
 
 int main(int argc, char *argv[])
 {
-   if ( argc != 5) {
-        std::cout << "Usage: " << argv[0] << "<fp32/fp16/bp16> <filename1> <filename2> <number of data>" << std::endl; 
+   if ( argc < 5) {
+        std::cout << "Usage: " << argv[0] << "<fp32/fp16/bp16> <file1> <file2> <number of data> <1/0 show accumulation>" << std::endl; 
 	throw std::runtime_error("Invalid argument!"); 
    }
 
@@ -21,14 +21,22 @@ int main(int argc, char *argv[])
    int num_elements = atoi(argv[4]); 
    float result = 0.0f; 
 
+   bool show_accumulation = false; 
+
+   if (argc > 5 && atoi(argv[5]) > 0) 
+       show_accumulation = true;
+
    for (int i=0; i < num_elements; i++) {
        float data1, data2; 
+       float data1_conved, data2_conved;
 
        f1 >> data1; 
        f2 >> data2; 
 
        if (precision == "fp32") {
           result += data1 * data2;        
+          data1_conved = data1; 
+	  data2_conved = data2; 
        }
        else 
        if (precision == "bp16") {
@@ -36,6 +44,8 @@ int main(int argc, char *argv[])
            bhalf_t val2 = type_convert<bhalf_t>(data2); 
 
            result += type_convert<float>(val1) * type_convert<float>(val2); 
+           data1_conved = type_convert<float>(val1); 
+	   data2_conved = type_convert<float>(val2);
        }
        else
        if (precision == "fp16") {
@@ -43,11 +53,16 @@ int main(int argc, char *argv[])
            half_t val2 = type_convert<half_t>(data2); 
 
            result += type_convert<float>(val1) * type_convert<float>(val2); 
+           data1_conved = type_convert<float>(val1); 
+	   data2_conved = type_convert<float>(val2);
        }
        else 
 	    throw std::runtime_error("Not supported type!"); 
 
-       std::cout << i << ": " << std::fixed << data1 << "  " << data2 << std::endl; 
+       if (show_accumulation) 
+           std::cout << i << ": " << std::fixed << result << std::endl;
+       else 
+           std::cout << i << ": " << std::fixed << data1_conved << "  " << data2_conved << std::endl; 
    }
 
    std::cout << "Dot product: " << result << std::endl; 
